f256058860f2b9f655848cf5016aa50c24fc0fb0
Amirhosein Rafeie <rafeieamirhosein@gmail.com>
2024-06-02 15:11:04 +0330

    config:  next router in vitest setup

--- src/__tests__/NavBar.test.tsx
+++ src/__tests__/NavBar.test.tsx
@@ -2,22 +2,6 @@ import { expect, test, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import NavBar from "../components/NavBar";
 
-const { useRouter, mockedRouterPush } = vi.hoisted(() => {
-  const mockedRouterPush = vi.fn();
-  return {
-    useRouter: () => ({ push: mockedRouterPush }),
-    mockedRouterPush,
-  };
-});
-
-vi.mock("next/navigation", async () => {
-  const actual = await vi.importActual("next/navigation");
-  return {
-    ...actual,
-    useRouter,
-  };
-});
-
 test("NavBar rendered without crash", () => {
   render(<NavBar />);
   const navbarElement = screen.getByTestId("nav");
--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-dom/vitest";
+
+const { useRouter, mockedRouterPush } = vi.hoisted(() => {
+  const mockedRouterPush = vi.fn();
+  return {
+    useRouter: () => ({ push: mockedRouterPush }),
+    mockedRouterPush,
+  };
+});
+
+vi.mock("next/navigation", async () => {
+  const actual = await vi.importActual("next/navigation");
+  return {
+    ...actual,
+    useRouter,
+  };
+});
